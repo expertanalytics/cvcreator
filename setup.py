747b6c1f219bb474bff9ec0ddc2baaf4f6623926
Jonathan Feinberg <jonathf@gmail.com>
2016-03-26 01:27:06 +0100

    renamed frontend to it's true name cvcreate{2,3}

--- setup.py
+++ setup.py
@@ -20,30 +20,31 @@ setup(
 
 if sys.version_info.major == 2:
     input = raw_input
-    src = "frontend2.py"
+    src = "cvcreate2.py"
 else:
-    src = "frontend3.py"
+    src = "cvcreate3.py"
 
 
 def copy_script():
 
-    q = input("[y]es/[n]o/[c]ustom location:")
-    q = q[:1].lower()
+    query = input("[y]es/[n]o/[c]ustom location:")
+    query = query[:1].lower()
 
-    if q == "y":
+    if query == "y":
         target = "/usr/local/bin/cvcreate"
+        shutil.copy(src, target)
 
-    elif q == "n":
+    elif query == "n":
         sys.exit(0)
 
-    elif q == "c":
+    elif query == "c":
         target = input("Input path:")
+        shutil.copy(src, target)
 
     else:
         print("please select valid option")
         copy_script()
 
-    shutil.copy(src, target)
 
 print("Copy cvcreate script to /usr/local/bin?")
 copy_script()
