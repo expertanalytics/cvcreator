e8ecd1b5bfc6f0f1d4e86fe10abe66d133b1df4e
Jonathan Feinberg <jonathf@gmail.com>
2017-02-08 10:13:30 +0100

    bugfix: allow use of file linking in setup
    Solves problem with running 'cvcreate --yaml'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ shutil.copy("config.yaml", "cvcreator/templates")
 
 setup(
     name="cvcreator",
-    version="0.4.2",
+    version="0.4.3",
     url="http://github.com/ExpertAnalytics/cvcreator",
 
     author="Jonathan Feinberg",
@@ -30,6 +30,7 @@ setup(
     install_requires=[
         "pyyaml",
     ],
+    zip_safe=False,
 )
 
 if "develop" not in sys.argv:
