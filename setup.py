e09619c211deb9b06c9bc1bdfc803367d3bf0447
Eivind Storm Aarnæs <eivind@xal.no>
2017-04-06 16:51:49 +0200

    Added support for 'Volume' and 'Period' fields for projects.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ shutil.copy("config.yaml", "cvcreator/templates")
 
 setup(
     name="cvcreator",
-    version="0.4.3",
+    version="0.4.4",
     url="http://github.com/ExpertAnalytics/cvcreator",
 
     author="Jonathan Feinberg",
