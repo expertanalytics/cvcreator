dae0df1b4593535c274cc50ea498f7944ed55385
Jonathan Feinberg <jonathf@gmail.com>
2021-05-24 16:14:10 +0200

    Removing redundant code.
    The refactoring made most code redundant.

--- cvcreator/__init__.py
+++ cvcreator/__init__.py
@@ -4,8 +4,3 @@ cvparser library
 
 A library for automated create CV on the fly.
 """
-
-from .parser import *     # noqa: ignore starred import here
-from .workspace import *  # noqa: ignore starred import here
-
-__all__ = parser.__all__ + workspace.__all__
