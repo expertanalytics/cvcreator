a491879d2c9cbd32aa796876bc15e1c5285dba87
Jonathan Feinberg <jonathf@gmail.com>
2021-06-15 15:03:18 +0200

    adding tests

--- cvcreator/tech_skills.py
+++ cvcreator/tech_skills.py
@@ -1,14 +1,20 @@
 """Group skills into categories."""
 import os
-from typing import Dict, List, Sequence
+from typing import Any, Dict, List, Sequence
 from collections import defaultdict
 
 import toml
 
 from .schema import TechnicalSkill
 
-
 CURDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_skills_data() -> Dict[str, Any]:
+    """Get technical skills data."""
+    with open(os.path.join(
+            CURDIR, "templates", "tech_skills.toml")) as handler:
+        return toml.load(handler)
 
 
 def make_skill_groups(
@@ -49,19 +55,17 @@ def make_skill_groups(
         {'Code management': ['CVS', 'Git', 'pip'], 'Tools': ['SQLite']}
 
     """
-    with open(os.path.join(
-            CURDIR, "templates", "tech_skills.toml")) as handler:
-        data = toml.load(handler)
+    skills_data = get_skills_data()
 
     # get general group label popularity
     count = defaultdict(int)
-    for skill in data["skills"]:
-        for label in data["skills"][skill]:
+    for skill in skills_data["skills"]:
+        for label in skills_data["skills"][skill]:
             count[label] += 1
     max_count = max(count.values())+1
 
     skills = list(skills)
-    unknown_skills = set(skills).difference(data["skills"])
+    unknown_skills = set(skills).difference(skills_data["skills"])
     assert not unknown_skills, (
             f"unrecognized technical skills: {sorted(unknown_skills)}")
 
@@ -71,7 +75,7 @@ def make_skill_groups(
         # make map from group label to skill
         mapping = defaultdict(list)
         for skill in skills:
-            for label in data["skills"][skill]:
+            for label in skills_data["skills"][skill]:
                 mapping[label].append(skill)
 
         # sort based on primery size of group and secondary general popularity
@@ -88,6 +92,6 @@ def make_skill_groups(
         skills = [skill for skill in skills if skill not in mapping[key]]
         output[key] = sorted(mapping.pop(key))
 
-    output = [TechnicalSkill(title=title, values=values)
-              for title, values in output.items()]
+    output = [TechnicalSkill(title=title, values=output[title])
+              for title in skills_data["allowed_labels"] if title in output]
     return output
---
+++ test/test_tech_skills.py
@@ -0,0 +1,36 @@
+import glob
+import os
+
+from pytest import fixture
+
+from cvcreator.tech_skills import get_skills_data
+
+CURDIR = os.path.dirname(os.path.abspath(__file__))
+CODEDIR = os.path.abspath(os.path.join(os.path.pardir, "cvcreator"))
+
+
+@fixture
+def skills_data():
+    return get_skills_data()
+
+
+def test_icon_reference(skills_data):
+    for iconpath in glob.glob(os.path.join(CODEDIR, "icons", "*.pdf")):
+        assert os.path.splitext(os.path.basename(iconpath))[0] in skills_data["skills"].keys(), (
+            f"Icon {iconpath} does not have a tech skill counterpart.")
+
+
+def test_skills_labels(skills_data):
+    unknown_labels = {
+        label for labels in skills_data["skills"].values()
+        for label in labels
+    }.difference(skills_data["allowed_labels"])
+    assert not unknown_labels, f"Label not recognized: {unknown_labels}"
+
+
+def test_short_maps(skills_data):
+    unknown_keys = set(skills_data["short_map"]).difference(skills_data["skills"])
+    assert not unknown_keys, f"Shortmap key not recognized: {unknown_keys}"
+    unknown_vals = set(skills_data["short_map"].values()).difference(skills_data["skills"])
+    assert not unknown_vals, f"Shortmap value not recognized: {unknown_vals}"
+
