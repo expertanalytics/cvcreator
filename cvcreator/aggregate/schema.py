bc88ef446299f352ecd9afb897e45ad42abf71ad
Jonathan Feinberg <jonathf@gmail.com>
2021-08-16 12:40:06 +0200

    expand agggregate features

--- cvcreator/aggregate/schema.py
+++ cvcreator/aggregate/schema.py
@@ -11,9 +11,23 @@ class SkillCount(BaseModel):
     count: int
 
 
+class TopicCount(BaseModel):
+    """Field of research and their count."""
+    value: str
+    count: int
+
+
 class AggregateContent(BaseModel):
     """Schema for Aggregate content file."""
 
     technical_skills: List[str]
+    topics: List[str]
+
+    num_employees: int = 0
+    num_doctors: int = 0
+    num_nationalities: int = 0
+    num_languages_spoken: int = 0
+    num_universities_attended: int = 0
 
     skill_count: List[SkillCount] = Field(default_factory=list)
+    topic_count: List[TopicCount] = Field(default_factory=list)
